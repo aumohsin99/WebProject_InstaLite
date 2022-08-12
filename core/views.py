6b895fe43ce98173d0ea43045fd9381a2def97b7
Moshin <bsef19m031@pucit.edu.pk>
2022-08-12 17:43:38 +0500

    templates addition

--- core/views.py
+++ core/views.py
@@ -3,4 +3,5 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return HttpResponse('<h1>Welcome to Social Book</h1>')
+    return render(request, 'index.html')
+   # return HttpResponse('<h1>Welcome to Social Book</h1>')
