60a7c279a9266b93c63b7ff651fc5d998f9d5a2d
Moshin <bsef19m031@pucit.edu.pk>
2022-08-12 23:45:01 +0500

    signup final

--- core/views.py
+++ core/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User, auth
 from django.contrib import  messages
+from .models import Profile
 from django.http import HttpResponse
 # Create your views here.
 
@@ -20,13 +21,19 @@ def signup(request):
                 messages.info(request, 'Email Taken')
                 return redirect('signup')
             elif User.objects.filter(username=username).exists():
-                messages.info(request, 'Userame Taken')
+                messages.info(request, 'Username Taken')
                 return redirect('signup')
             else:
                 user = User.objects.create_user(username=username, email=email, password=password)
                 user.save()
 
+                #login user and redirect to settings page
+                # create a profile for new user
 
+                user_model = User.objects.get(username=username)
+                new_profile = Profile.objects.create(user=user_model, id_user=user_model.id)
+                new_profile.save()
+                return redirect('signup')
         else:
             messages.info(request, 'Password not matching')
             return redirect('signup')
