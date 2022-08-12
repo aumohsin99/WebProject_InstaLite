5efb1fcfa08ce04aaddebabefa3b0e3f3001b4d0
Moshin <bsef19m031@pucit.edu.pk>
2022-08-13 00:23:35 +0500

    login complete now

--- core/views.py
+++ core/views.py
@@ -61,7 +61,7 @@ def signin(request):
     else:
         return render(request, 'signin.html')
 
-
+@login_required(login_url='signin')
 def logout(request):
     auth.logout(request)
     return redirect('signin')
