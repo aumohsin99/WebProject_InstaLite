43d5a83964e63d2f3ed48e8fe90074c5fa442907
Moshin <bsef19m031@pucit.edu.pk>
2022-08-13 00:21:36 +0500

    logout validation

--- core/views.py
+++ core/views.py
@@ -2,9 +2,12 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.models import User, auth
 from django.contrib import  messages
 from .models import Profile
+from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
 # Create your views here.
 
+# decorator is used in the line below
+@login_required(login_url='signin')
 def index(request):
     return render(request, 'index.html')
    # return HttpResponse('<h1>Welcome to Social Book</h1>')
@@ -57,3 +60,9 @@ def signin(request):
             return redirect('signin')
     else:
         return render(request, 'signin.html')
+
+
+def logout(request):
+    auth.logout(request)
+    return redirect('signin')
+
