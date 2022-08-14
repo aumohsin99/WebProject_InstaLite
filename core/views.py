a4183b435c97e93cf983a305ef542cb4d6f49047
Moshin <bsef19m031@pucit.edu.pk>
2022-08-14 22:20:16 +0500

    all pictures showing on feed error to be resolved and video at 04 17 48

--- core/views.py
+++ core/views.py
@@ -4,15 +4,28 @@ from django.contrib import messages
 from .models import Profile, Post, LikePost, FollowersCount
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
+from itertools import chain
 
 # Create your views here.
 
 # decorator is used in the line below
 @login_required(login_url='signin')
 def index(request):
-    user_object = User.objects.get(username = request.user.username)
+    user_object = User.objects.get(username=request.user.username)
     user_profile = Profile.objects.get(user=user_object)
     # first get object of user profile and then get image from that object
+
+    user_following_list = []
+    feed = []
+    user_following = FollowersCount.objects.filter(follower=request.user.username)
+
+    for users in user_following:
+        user_following_list.append(users.user)
+    for usernames in user_following_list:
+        feed_lists = Post.objects.filter(user=usernames)
+        feed.append(feed_lists)
+
+    feed_list = list(chain(*feed))
 
 
     # followed person posts only
@@ -20,9 +33,9 @@ def index(request):
     posts = Post.objects.all()
     return render(request, 'index.html', {'user_profile': user_profile, 'posts': posts})
 
+
 @login_required(login_url='signin')
 def upload(request):
-
     if request.method == 'POST':
         # user = User.objects.get(user.username)
         user = request.user.username
@@ -36,11 +49,12 @@ def upload(request):
     else:
         return redirect('/')
 
+
 @login_required(login_url='signin')
 def settings(request):
     user_profile = Profile.objects.get(user=request.user)
     if request.method == 'POST':
-        if request.FILES.get('image')==None:
+        if request.FILES.get('image') == None:
             image = user_profile.profileimg
             bio = request.POST['bio']
             location = request.POST['location']
@@ -50,11 +64,10 @@ def settings(request):
             user_profile.location = location
             user_profile.save()
 
-        if request.FILES.get('image') !=None:
+        if request.FILES.get('image') != None:
             image = request.FILES.get('image')
             bio = request.POST['bio']
             location = request.POST['location']
-
 
             user_profile.profileimg = image
             user_profile.bio = bio
@@ -64,6 +77,7 @@ def settings(request):
         return redirect('settings')
 
     return render(request, 'setting.html', {'user_profile': user_profile})
+
 
 def signup(request):
     if request.method == 'POST':
@@ -83,7 +97,7 @@ def signup(request):
                 user = User.objects.create_user(username=username, email=email, password=password)
                 user.save()
 
-                #login user and redirect to settings page
+                # login user and redirect to settings page
                 user_login = auth.authenticate(username=username, password=password)
                 auth.login(request, user_login)
 
@@ -115,6 +129,7 @@ def signin(request):
     else:
         return render(request, 'signin.html')
 
+
 @login_required(login_url='signin')
 def logout(request):
     auth.logout(request)
@@ -133,14 +148,15 @@ def like_post(request):
     if like_filter == None:
         new_like = LikePost.objects.create(post_id=post_id, username=username)
         new_like.save()
-        post.no_of_likes = post.no_of_likes+1
+        post.no_of_likes = post.no_of_likes + 1
         post.save()
         return redirect('/')
     else:
         like_filter.delete()
-        post.no_of_likes = post.no_of_likes-1
+        post.no_of_likes = post.no_of_likes - 1
         post.save()
         return redirect('/')
+
 
 @login_required(login_url='signin')
 def profile(request, pk):
@@ -171,6 +187,7 @@ def profile(request, pk):
     }
     return render(request, 'profile.html', context)
 
+
 def follow(request):
     if request.method == 'POST':
         follower = request.POST['follower']
@@ -179,11 +196,11 @@ def follow(request):
         if FollowersCount.objects.filter(follower=follower, user=user).first():
             delete_follower = FollowersCount.objects.get(follower=follower, user=user)
             delete_follower.delete()
-            return redirect('/profile/'+user)
+            return redirect('/profile/' + user)
         else:
             new_follower = FollowersCount.objects.create(follower=follower, user=user)
             new_follower.save()
-            return redirect('/profile/'+user)
+            return redirect('/profile/' + user)
 
     else:
         return redirect('/')
